8ffccd4769184b19859f4c5b1d9039e20c038ae9
xephtar <manger2105@hotmail.com>
2019-10-09 19:52:46 +0300

    db initiliaze
    trying to initialize db with dummy way

--- server.py
+++ server.py
@@ -1,7 +1,30 @@
-from flask import Flask
+import psycopg2
+import os
+from flask import Flask, redirect, url_for
+from flask import jsonify
+
+
+url = os.getenv("DATABASE_URL")
 
 
 app = Flask(__name__)
+
+con = psycopg2.connect(url)
+
+# cursor
+cur = con.cursor()
+
+
+def print_dummy():
+    cur.execute("select * from dummy")
+    rows = cur.fetchall()
+    return rows
+
+
+@app.route("/dummy/", methods=['GET'])
+def show_dummy():
+    rows = print_dummy()
+    return jsonify(rows)
 
 
 @app.route("/")
@@ -9,5 +32,10 @@ def home_page():
     return "Initializing........"
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
+
+# close the cursor
+cur.close()
+# close the connection
+con.close()
