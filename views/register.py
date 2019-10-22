3fe73789c859f79ed2a81eee62aff7b012cf3d6f
xephtar <manger2105@hotmail.com>
2019-10-22 22:38:56 +0300

    Updated the name of a directory

--- views/register.py
+++ views/register.py
@@ -3,31 +3,7 @@ from flask import render_template
 
 
 def register_page():
-    html = """
-            {% block title %}Movie list{% endblock %}
-        {% block content %}
-            <h1 class="title">Movies</h1>
-        
-            {% if vets_list %}
-            <table class="table is-striped is-fullwidth">
-              {% for id, vet in vets_list %}
-              <tr>
-                <td>
-                  {{ vet.name }}
-                  {% if vet.age %} ({{ vet.age }}) {% endif %}
-                </td>
-              </tr>
-              {% endfor %}
-            <select name="vets">
-                {% for id, vet in vets_list %}
-                    <option value="{{ id }}">{{ vet }}</option>
-                {% endfor %}
-            </select>
-            </table>
-            {% endif %}
-        {% endblock %}
-    """
     v = Vets.get(id=id)
     if v:
         _vets_list = v.__dict__
-        return render_template(html, vets_list=sorted(_vets_list))
+        return render_template('register.html', vets_list=sorted(_vets_list))
