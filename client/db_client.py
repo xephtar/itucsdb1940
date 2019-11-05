8ff9b800eec7fe31f8758eddbd11ed5c73c5b4fd
Omer Faruk Davarcı <davarci16@itu.edu.tr>
2019-11-05 18:49:30 +0300

    Fixed localization things

--- client/db_client.py
+++ client/db_client.py
@@ -5,7 +5,7 @@ import psycopg2 as db
 class Client(object):
 
     def __init__(self):
-        self.dsn = 'postgres://sxmxcsbbhbitaq:374c6268b10c0d0181454a0dc010c322b7dfe53e173b05e7cfdd64ee5d7e042d@ec2-54-217-234-157.eu-west-1.compute.amazonaws.com:5432/det2ikhqat3u6m'
+        self.dsn = os.getenv('DATABASE_URL')
         self.connection = db.connect(self.dsn)
         self.cursor = self.connection.cursor()
 
