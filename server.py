954df4ec46aa4f003307ba62f173cf639b45ca29
Omer Faruk Davarci <davarci16@itu.edu.tr>
2019-12-13 03:16:04 +0300

    Updated load_user method

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
-from flask import Flask
+from flask import Flask, session, request
 from flask_restful import Api
-
 from views.users import get_user
 from views.vets import VetsAPI, VetsListAPI
 from views.owners import OwnersAPI, OwnersListAPI
@@ -15,13 +14,25 @@ lm = LoginManager()
 
 @lm.user_loader
 def load_user(user_id):
-    user = get_user(user_id)
-    return user
+    if not session.get("token") is None:
+        return get_user(session.get("user_id"))
+
+    if user_id is not None:
+        return get_user(user_id)
+    return None
 
 
 app = Flask(__name__, template_folder='template')
 app.secret_key = secrets.token_urlsafe(16)
 api = Api(app)
+lm.init_app(app)
+lm.login_view = "login_page"
+
+
+@app.before_request
+def before_request():
+    user = current_user
+
 
 api.add_resource(VetsAPI, '/vets/<int:id>')
 api.add_resource(VetsListAPI, '/vets/')
@@ -33,8 +44,4 @@ app.add_url_rule("/", view_func=home_page)
 app.add_url_rule("/login", view_func=login_page, methods=["GET", "POST"])
 app.add_url_rule("/logout", view_func=logout_page, methods=["GET"])
 
-lm.init_app(app)
-lm.login_view = "login_page"
-
 if __name__ == '__main__':
-    app.run(debug=False)
--- views/users.py
+++ views/users.py
@@ -3,5 +3,7 @@ from models.users import Users
 
 def get_user(user_id):
     u = Users.get(username=user_id)
-    user = Users(user_id, u[2]) if u[2] else None
-    return user
+    if u:
+        user = Users(user_id, u[2]) if u[2] else None
+        return user
+    return None
