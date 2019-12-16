a93844ac3f7f9c0d94fc1c5b690fd170bbc193f9
Omer Faruk Davarci <davarci16@itu.edu.tr>
2019-12-16 22:47:14 +0300

    Added new views and organized CRUD operations

--- views/owners.py
+++ views/owners.py
@@ -1,9 +1,7 @@
 from flask_login import login_required, current_user
 from flask_restful import reqparse, Resource, abort
-from flask import redirect, flash, session, request, url_for
+from flask import redirect, flash, request, url_for
 from models.owners import Owners
-from models.vets_to_owners import Vets_to_Owners
-from views.url import home_page
 
 
 class OwnersAPI(Resource):
@@ -13,6 +11,30 @@ class OwnersAPI(Resource):
         self.parser = reqparse.RequestParser()
         self.parser.add_argument('name', type=str)
         self.parser.add_argument('age', type=str)
+        self.parser.add_argument('address', type=str)
+        self.parser.add_argument('gender', type=str)
+        self.parser.add_argument('treatments', type=str)
+
+    def post(self, phonenumber):
+        method = reqparse.RequestParser()
+        method.add_argument('_method', type=str)
+        method = method.parse_args()
+        if method['_method'] == "Delete":
+            if OwnersAPI.delete(self, phonenumber):
+                flash("Success, Owner is deleted!")
+            else:
+                flash("Fail, Owner is not deleted!")
+            next_page = request.args.get("next", url_for("home_page"))
+            return redirect(next_page)
+        elif method['_method'] == "Update":
+            if OwnersAPI.put(self, phonenumber):
+                flash("Success, Owner is updated!")
+            else:
+                flash("Fail, Owner is not updated!")
+            next_page = request.args.get("next", url_for("home_page"))
+            return redirect(next_page)
+        else:
+            abort(405)
 
     def get(self, phonenumber):
         u = Owners.get(phonenumber=phonenumber)
@@ -66,29 +88,14 @@ class OwnersListAPI(Resource):
         args = self.parser.parse_args()
         if args:
             u = Owners.create(**args)
-            if u:
+            if u is Owners:
                 owner = Owners.filter(**args).__getitem__(0)
-                return redirect('/owners/{}'.format(owner.phonenumber))
+                flash('You were successfully created Owner!')
+                flash(owner.name)
+                next_page = request.args.get("next", url_for("home_page"))
+                return redirect(next_page)
+            else:
+                flash('You were failed to create Owner!')
+                next_page = request.args.get("next", url_for("home_page"))
+                return redirect(next_page)
         return {}, 404
-        # args = self.parser.parse_args()
-        # vet = reqparse.RequestParser()
-        # vet.add_argument('phonenumber', type=str)
-        # vet.add_argument('vet_id', type=int)
-        # vet = vet.parse_args()
-        # print(args)
-        # print(vet)
-        # if args:
-        #     u = Owners.create(**args)
-        #     ovr = Vets_to_Owners.create(**vet)
-        #     if u == '404':
-        #         flash('You were failed to create Owner!')
-        #     else:
-        #         flash('You were successfully created Owner!')
-        #
-        #     if ovr == '404':
-        #         flash('You were failed to add Vet!')
-        #     else:
-        #         flash('You were successfully added Vet!')
-        #     next_page = request.args.get("next", url_for("home_page"))
-        #     return redirect(next_page)
-        # return {}, 404
